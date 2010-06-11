1ea784f93392ee9862bea1d7973e288a21cec6a5
Michael Howitz <mh@gocept.com>
2010-06-11 17:41:02 +0000

    fix namespace

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@
 # When developing and releasing this package, please follow the documented
 # Zope Toolkit policies as described by this documentation.
 ##############################################################################
-"""Setup for zope.app.preference package
+"""Setup for zope.preference package
 
 $Id$
 """
@@ -26,22 +26,22 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-setup(name = 'zope.app.preference',
-      version='3.7.1dev',
+setup(name = 'zope.preference',
+      version='3.8.0dev',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='User Preferences Framework',
       long_description=(
           read('README.txt')
           + '\n\n' +
-          'Detailed Dcoumentation\n' +
+          'Detailed Documentation\n' +
           '======================\n'
           + '\n\n' +
-          read('src', 'zope', 'app', 'preference', 'README.txt')
+          read('src', 'zope', 'preference', 'README.txt')
           + '\n\n' +
           read('CHANGES.txt')
           ),
-      keywords = "zope3 user preference",
+      keywords = "bluebream zope zope3 user preference",
       classifiers = [
           'Development Status :: 5 - Production/Stable',
           'Environment :: Web Environment',
@@ -52,11 +52,11 @@ setup(name = 'zope.app.preference',
           'Operating System :: OS Independent',
           'Topic :: Internet :: WWW/HTTP',
           'Framework :: Zope3'],
-      url='http://pypi.python.org/pypi/zope.app.preference',
+      url='http://pypi.python.org/pypi/zope.preference',
       license='ZPL 2.1',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
-      namespace_packages=['zope', 'zope.app'],
+      namespace_packages=['zope'],
       extras_require=dict(test=[
           'zope.app.testing',
           'zope.app.zcmlfiles',
