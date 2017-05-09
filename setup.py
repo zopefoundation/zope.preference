802010c06ca611fdb56c9bed4dcbe0da84a75af9
Jason Madden <jamadden@gmail.com>
2017-05-09 11:20:26 -0500

    Back to development: 4.0.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ tests_require = [
 ]
 
 setup(name='zope.preference',
-      version='4.0.0',
+      version='4.0.1.dev0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='User Preferences Framework',
